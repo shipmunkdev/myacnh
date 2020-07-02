299f7633f2282a54ecb65b3eb44f37bc4a31f236
Sein Tun <contact@seintun.dev>
2020-07-01 17:05:14 -0700

    ACNH-6: Created Search box for filtering VillagerCards with matching keywords (#4)

---
+++ src/components/SearchBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SearchBox = ({ searchChange }) => {
+    return (
+        <input
+            className='pa3 ma2 w-80 ba br-pill b--green bg-lightest-blue'
+            type='search'
+            placeholder='Search villagers by name'
+            onChange={searchChange}
+        />
+    );
+};
+
+export default SearchBox;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 import VillagersList from '../components/VillagersList';
+import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import { villagers } from '../villagers';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            villagers: [],
+            searchField: ''
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ villagers });
+    }
+
+    onSearchChange = e => {
+        this.setState({ searchField: e.target.value });
+    };
+
     render() {
+        const { villagers, searchField } = this.state;
+        const filteredvillagers = villagers.filter(villager => {
+            return villager.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+
         return !villagers.length
             ? <h1>Flying with Dodo Airlines . . .</h1>
             : (
                 <div className='tc'>
                     <div className='bg-light-green'>
                         <h1 className='f4 ma1 blue'>myACNH.online</h1>
+                        <SearchBox searchChange={this.onSearchChange} />
                     </div>
                     <Scroll>
                         <ErrorBoundry>
-                            <VillagersList villagers={villagers} />
+                            <VillagersList villagers={filteredvillagers} />
                         </ErrorBoundry>
                     </Scroll>
                     <div>
